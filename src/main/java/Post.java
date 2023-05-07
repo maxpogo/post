be16e1bea2be1f2bc2549f8fe9c5704188442b27
maxpogo <maxim.pogodaev.93@mail.ru>
2023-05-07 12:44:58 +0300

    first commit

---
+++ src/main/java/Post.java
@@ -0,0 +1,19 @@
+public class Post {
+    public String name;
+
+    public String passport;
+
+    public String patronymic;
+
+    public String phone;
+
+    public String surname;
+
+    public Boolean subscription;
+
+
+    public FormDate birthday;
+
+
+
+}
